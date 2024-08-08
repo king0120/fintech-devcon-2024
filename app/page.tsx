ad5415c01d9c2c7fc1ef3a7a1d432eb5edf92c74
Jamie King <jamieking@flashtract.com>
2024-08-08 13:14:20 -0500

    include keynote presentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import {TransactionDetails} from "@/components/TransactionDetails";
 import {TransactionList} from "@/components/TransactionList";
+import {Suspense} from "react";
 
 export default function Home({searchParams}: { searchParams: Record<string, string> }) {
     // NextJS Specific
@@ -9,7 +10,9 @@ export default function Home({searchParams}: { searchParams: Record<string, stri
         <section className="flex">
             <TransactionList transactionId={transactionId}/>
             <div className={'bg-grey-200 min-h-screen w-3/4'}>
-                <TransactionDetails transactionId={transactionId}/>
+                <Suspense key={transactionId} fallback={<div>Loading...</div>}>
+                    <TransactionDetails transactionId={transactionId}/>
+                </Suspense>
             </div>
         </section>
     );
