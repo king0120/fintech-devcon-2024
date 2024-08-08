d5df4e1aef41bbfa78b6d8e88639ccec925fecd2
Jamie King <jamieking@flashtract.com>
2024-08-08 10:44:52 -0500

    in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import {TransactionDetails} from "@/components/TransactionDetails";
+import {TransactionList} from "@/components/TransactionList";
+
 export default function Home({searchParams}: { searchParams: Record<string, string> }) {
     // NextJS Specific
     const transactionId = Number(searchParams.transaction)
 
     return (
         <section className="flex">
-            {/*TRANSACTION LIST HERE*/}
+            <TransactionList transactionId={transactionId}/>
             <div className={'bg-grey-200 min-h-screen w-3/4'}>
                 {/*TRANSACTION DETAILS HERE*/}
             </div>
--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -1,8 +1,11 @@
 import {TransactionProps} from "@/lib/types";
 import {CalendarDaysIcon} from "@/components/icons/CalendarDaysIcon";
 import {ScrollArea} from "@/components/ui/scroll-area";
+import {db} from "@/db";
+import {transactions} from "@/db/schema";
 
 export const TransactionList = async ({transactionId}: TransactionProps) => {
+    const data = await db.select().from(transactions);
     return (
         <aside className="min-h-screen w-1/4 bg-muted/40 border-r px-4 py-6 flex flex-col gap-4">
             {/* Create transaction button should go here*/}
@@ -12,6 +15,7 @@ export const TransactionList = async ({transactionId}: TransactionProps) => {
             </div>
             <ScrollArea className="flex flex-col gap-2 max-h-[500px]">
                 {/*    List of transactions should go here*/}
+                {JSON.stringify(data, null, 2)}
             </ScrollArea>
         </aside>
     )
